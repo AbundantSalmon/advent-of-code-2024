c307103821a7db0a5b6f8ede1482b8609e0c220e
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2024-12-03 18:28:21 +0800

    day 3 part 1

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,67 +1,198 @@
-﻿namespace Day03;
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
 
 internal static class Day03
 {
-	public static void Main(string[] args)
-	{
-		const string inputFile = "input.txt";
+    public static void Main(string[] args)
+    {
+        const string inputFile = "input.txt";
 
-		Console.WriteLine("Day03!");
+        Console.WriteLine("Day03!");
 
-		List<string> lines = [];
+        List<string> lines = [];
 
-		try
-		{
-			StreamReader sr = new(inputFile);
-			string? line = sr.ReadLine();
-			while (line != null)
-			{
-				lines.Add(line);
-				line = sr.ReadLine();
-			}
+        try
+        {
+            StreamReader sr = new(inputFile);
+            string? line = sr.ReadLine();
+            while (line != null)
+            {
+                lines.Add(line);
+                line = sr.ReadLine();
+            }
 
-			sr.Close();
-		}
-		catch (Exception e)
-		{
-			Console.WriteLine("Exception: " + e.Message);
-		}
+            sr.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
 
-		if (args.Length != 1)
-		{
-			Console.WriteLine("Usage: Day01 <part 1/2>");
-			return;
-		}
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: Day01 <part 1/2>");
+            return;
+        }
 
-		string input = args[0];
-		int day = int.Parse(input);
-		switch (day)
-		{
-			case 1:
-				Part1(lines);
-				break;
-			case 2:
-				Part2(lines);
-				break;
-			default:
-				Console.WriteLine("Usage: Day01 <part 1/2");
-				break;
-		}
-	}
+        string input = args[0];
+        int day = int.Parse(input);
+        switch (day)
+        {
+            case 1:
+                Part1(lines);
+                break;
+            case 2:
+                Part2(lines);
+                break;
+            default:
+                Console.WriteLine("Usage: Day01 <part 1/2");
+                break;
+        }
+    }
 
-	private static void Part1(List<string> lines)
-	{
-		Console.WriteLine("Part 1");
+    private static void Part1(List<string> lines)
+    {
+        Console.WriteLine("Part 1");
 
-		foreach (string line in lines)
-		{
-			Console.WriteLine(line);
-		}
-	}
+        Regex mulOpen = new Regex(@"mul\([0-9]*$");
+        Regex mulFirstNum = new Regex(@"mul\([0-9]*,$");
+        Regex mulSecondNum = new Regex(@"mul\([0-9]*,[0-9]*$");
+        Regex mulSolution = new Regex(@"mul\(([0-9]*),([0-9]*)\)");
 
-	private static void Part2(List<string> lines)
-	{
-		Console.WriteLine("Part 2");
 
-	}
+        int sum = 0;
+
+        foreach (string line in lines)
+        {
+            string currentToken = "";
+            foreach (char characte in line)
+            {
+                string character = characte.ToString();
+
+                if (currentToken == "")
+                {
+                    if (character == "m")
+                    {
+                        currentToken += character;
+                    }
+
+                    continue;
+                }
+                else if (currentToken == "m")
+                {
+                    if (character == "u")
+                    {
+                        currentToken += character;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (currentToken == "mu")
+                {
+                    if (character == "l")
+                    {
+                        currentToken += character;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (currentToken == "mul")
+                {
+                    if (character == "(")
+                    {
+                        currentToken += character;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (currentToken == "mul(")
+                {
+                    if (int.TryParse(character, out int num))
+                    {
+                        currentToken += num;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (mulOpen.Match(currentToken).Success)
+                {
+                    if (character == ",")
+                    {
+                        currentToken += character;
+                    }
+                    else if (int.TryParse(character, out int num))
+                    {
+                        currentToken += num;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (mulFirstNum.Match(currentToken).Success)
+                {
+                    if (int.TryParse(character, out int num))
+                    {
+                        currentToken += num;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else if (mulSecondNum.Match(currentToken).Success)
+                {
+                    if (character == ")")
+                    {
+                        currentToken += character;
+                        Match match = mulSolution.Match(currentToken);
+                        sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                        currentToken = "";
+                    }
+                    else if (int.TryParse(character, out int num))
+                    {
+                        currentToken += num;
+                    }
+                    else
+                    {
+                        currentToken = "";
+                    }
+
+                    continue;
+                }
+                else
+                {
+                    throw new Exception("Unexpected currentToken: " + currentToken);
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static void Part2(List<string> lines)
+    {
+        Console.WriteLine("Part 2");
+    }
 }
