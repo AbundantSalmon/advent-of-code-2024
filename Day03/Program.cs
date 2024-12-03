7864f9924d5d25b74c3028c8adc6574d0a82b4f1
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2024-12-03 19:51:40 +0800

    day 3 part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 
 namespace Day03;
 
@@ -194,5 +195,161 @@ internal static class Day03
     private static void Part2(List<string> lines)
     {
         Console.WriteLine("Part 2");
+
+        Regex mulOpen = new Regex(@"mul\([0-9]*$");
+        Regex mulFirstNum = new Regex(@"mul\([0-9]*,$");
+        Regex mulSecondNum = new Regex(@"mul\([0-9]*,[0-9]*$");
+        Regex mulSolution = new Regex(@"mul\(([0-9]*),([0-9]*)\)");
+
+        int sum = 0;
+
+        string line = "";
+        lines.ForEach((lineValue) => { line += lineValue; });
+
+        bool first = true;
+        string[] dontSegments = line.Split("don't()");
+        foreach (string seg in dontSegments)
+        {
+            List<string> list;
+            if (first)
+            {
+                // first line segment is always do
+                first = false;
+                list = [seg];
+            }
+            else
+            {
+                string[] doSeg = seg.Split("do()");
+                list = doSeg.ToList();
+                list.RemoveAt(0);
+            }
+
+            foreach (var thing in list)
+            {
+                string currentToken = "";
+                foreach (char characte in thing)
+                {
+                    string character = characte.ToString();
+
+                    if (currentToken == "")
+                    {
+                        if (character == "m")
+                        {
+                            currentToken += character;
+                        }
+
+                        continue;
+                    }
+                    else if (currentToken == "m")
+                    {
+                        if (character == "u")
+                        {
+                            currentToken += character;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (currentToken == "mu")
+                    {
+                        if (character == "l")
+                        {
+                            currentToken += character;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (currentToken == "mul")
+                    {
+                        if (character == "(")
+                        {
+                            currentToken += character;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (currentToken == "mul(")
+                    {
+                        if (int.TryParse(character, out int num))
+                        {
+                            currentToken += num;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (mulOpen.Match(currentToken).Success)
+                    {
+                        if (character == ",")
+                        {
+                            currentToken += character;
+                        }
+                        else if (int.TryParse(character, out int num))
+                        {
+                            currentToken += num;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (mulFirstNum.Match(currentToken).Success)
+                    {
+                        if (int.TryParse(character, out int num))
+                        {
+                            currentToken += num;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else if (mulSecondNum.Match(currentToken).Success)
+                    {
+                        if (character == ")")
+                        {
+                            currentToken += character;
+                            Match match = mulSolution.Match(currentToken);
+                            sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                            currentToken = "";
+                        }
+                        else if (int.TryParse(character, out int num))
+                        {
+                            currentToken += num;
+                        }
+                        else
+                        {
+                            currentToken = "";
+                        }
+
+                        continue;
+                    }
+                    else
+                    {
+                        throw new Exception("Unexpected currentToken: " + currentToken);
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
     }
 }
