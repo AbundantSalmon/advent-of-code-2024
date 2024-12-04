bd4b1262c31713cf3c0edc7e872455595b4a254c
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2024-12-04 14:45:23 +0800

    day 4 part 1

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -53,11 +53,225 @@ internal static class Day04
     {
         Console.WriteLine("Part 1");
 
+        List<List<char>> grid = []; // y,x
+
+
+        for (int y = 0; y < lines.Count; y++)
+        {
+            string line = lines[y];
+            grid.Add([]);
+            for (int x = 0; x < line.Length; x++)
+            {
+                grid[y].Add(line[x]);
+
+            }
+        }
+
+        int sum = 0;
+
+        for (int y = 0; y < grid.Count; y++)
+        {
+            var row = grid[y];
+            for (int x = 0; x < row.Count; x++)
+            {
+
+                char value = row[x];
+
+                if (value != 'X')
+                {
+                    continue;
+                }
+
+                // forwards horizontal
+                if (x >= 0 && x <= row.Count - 4)
+                {
+                    if (row[x + 1] == 'M' && row[x + 2] == 'A' && row[x + 3] == 'S')
+                    {
+                        sum++;
+                    }
+                }
+                // backwards horizontal
+                if (x >= 3 && x <= row.Count)
+                {
+                    if (row[x - 1] == 'M' && row[x - 2] == 'A' && row[x - 3] == 'S')
+                    {
+                        sum++;
+                    }
+                }
+
+                // forwards vertical
+                if (y >= 0 && y <= grid.Count - 4)
+                {
+                    if (grid[y + 1][x] == 'M' && grid[y + 2][x] == 'A' && grid[y + 3][x] == 'S')
+                    {
+                        sum++;
+                    }
+                }
+
+                // backwards vertical
+                if (y >= 3 && y <= grid.Count)
+                {
+                    if (grid[y - 1][x] == 'M' && grid[y - 2][x] == 'A' && grid[y - 3][x] == 'S')
+                    {
+                        sum++;
+                    }
+                }
+
+                // down-right
+                if (x >= 0 && x <= row.Count - 4 && y >= 0 && y <= grid.Count - 4)
+                {
+                    if (grid[y + 1][x + 1] == 'M' && grid[y + 2][x + 2] == 'A' && grid[y + 3][x + 3] == 'S')
+                    {
+                        sum++;
+                    }
+
+                }
+
+                // down-left
+                if (x >= 3 && x <= row.Count && y >= 0 && y <= grid.Count - 4)
+                {
+                    if (grid[y + 1][x - 1] == 'M' && grid[y + 2][x - 2] == 'A' && grid[y + 3][x - 3] == 'S')
+                    {
+                        sum++;
+                    }
+
+                }
+
+                // up-right
+                if (x >= 0 && x <= row.Count - 4 && y >= 3 && y <= grid.Count)
+                {
+                    if (grid[y - 1][x + 1] == 'M' && grid[y - 2][x + 2] == 'A' && grid[y - 3][x + 3] == 'S')
+                    {
+                        sum++;
+                    }
+
+                }
+
+                // up-left
+                if (x >= 3 && x <= row.Count && y >= 3 && y <= grid.Count)
+                {
+                    if (grid[y - 1][x - 1] == 'M' && grid[y - 2][x - 2] == 'A' && grid[y - 3][x - 3] == 'S')
+                    {
+                        sum++;
+                    }
+
+                }
+
+            }
+        }
+        Console.WriteLine(sum);
     }
 
     private static void Part2(List<string> lines)
     {
         Console.WriteLine("Part 2");
 
+        List<List<char>> grid = []; // y,x
+
+
+        for (int y = 0; y < lines.Count; y++)
+        {
+            string line = lines[y];
+            grid.Add([]);
+            for (int x = 0; x < line.Length; x++)
+            {
+                grid[y].Add(line[x]);
+
+            }
+        }
+
+        int sum = 0;
+
+        for (int y = 0; y < grid.Count; y++)
+        {
+            var row = grid[y];
+            for (int x = 0; x < row.Count; x++)
+            {
+
+                // char value = row[x];
+
+                // if (value != 'X')
+                // {
+                //     continue;
+                // }
+
+                // // forwards horizontal
+                // if (x >= 0 && x <= row.Count - 4)
+                // {
+                //     if (row[x + 1] == 'M' && row[x + 2] == 'A' && row[x + 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+                // }
+                // // backwards horizontal
+                // if (x >= 3 && x <= row.Count)
+                // {
+                //     if (row[x - 1] == 'M' && row[x - 2] == 'A' && row[x - 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+                // }
+
+                // // forwards vertical
+                // if (y >= 0 && y <= grid.Count - 4)
+                // {
+                //     if (grid[y + 1][x] == 'M' && grid[y + 2][x] == 'A' && grid[y + 3][x] == 'S')
+                //     {
+                //         sum++;
+                //     }
+                // }
+
+                // // backwards vertical
+                // if (y >= 3 && y <= grid.Count)
+                // {
+                //     if (grid[y - 1][x] == 'M' && grid[y - 2][x] == 'A' && grid[y - 3][x] == 'S')
+                //     {
+                //         sum++;
+                //     }
+                // }
+
+                // // down-right
+                // if (x >= 0 && x <= row.Count - 4 && y >= 0 && y <= grid.Count - 4)
+                // {
+                //     if (grid[y + 1][x + 1] == 'M' && grid[y + 2][x + 2] == 'A' && grid[y + 3][x + 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+
+                // }
+
+                // // down-left
+                // if (x >= 3 && x <= row.Count && y >= 0 && y <= grid.Count - 4)
+                // {
+                //     if (grid[y + 1][x - 1] == 'M' && grid[y + 2][x - 2] == 'A' && grid[y + 3][x - 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+
+                // }
+
+                // // up-right
+                // if (x >= 0 && x <= row.Count - 4 && y >= 3 && y <= grid.Count)
+                // {
+                //     if (grid[y - 1][x + 1] == 'M' && grid[y - 2][x + 2] == 'A' && grid[y - 3][x + 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+
+                // }
+
+                // // up-left
+                // if (x >= 3 && x <= row.Count && y >= 3 && y <= grid.Count)
+                // {
+                //     if (grid[y - 1][x - 1] == 'M' && grid[y - 2][x - 2] == 'A' && grid[y - 3][x - 3] == 'S')
+                //     {
+                //         sum++;
+                //     }
+
+                // }
+
+            }
+        }
+        Console.WriteLine(sum);
     }
 }
