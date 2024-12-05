09e165b2ec989e3df5bcb3cb7cee265a2b2f8d48
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2024-12-05 17:23:18 +0800

    day 5 part 2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -11,6 +11,38 @@ record Rule
 		this.after = after;
 	}
 }
+
+class RuleComparer : IComparer<int>
+{
+	private readonly IEnumerable<Rule> _rules;
+
+	public RuleComparer(IEnumerable<Rule> rules)
+	{
+		_rules = rules;
+	}
+
+	public int Compare(int x, int y)
+	{
+		if (x == y)
+		{
+			return 0;
+		}
+		foreach (var rule in _rules)
+		{
+			if (rule.after == x && rule.before == y)
+			{
+				return 1;
+
+			}
+			else if (rule.after == y && rule.before == x)
+			{
+				return -1;
+			}
+
+		}
+		return 0;
+	}
+}
 internal static class Day05
 {
 	public static void Main(string[] args)
@@ -154,6 +186,104 @@ internal static class Day05
 	private static void Part2(List<string> lines)
 	{
 		Console.WriteLine("Part 2");
+		Dictionary<int, List<Rule>> rules = new();
 
+		int sum = 0;
+
+		bool haveAllRules = false;
+		foreach (string line in lines)
+		{
+			if (line == "")
+			{
+				haveAllRules = true;
+				continue;
+			}
+
+			if (haveAllRules)
+			{
+				var pageNumberUpdatesStrings = line.Split(",");
+				List<int> pageNumberUpdates = [];
+				foreach (var value in pageNumberUpdatesStrings)
+				{
+					pageNumberUpdates.Add(int.Parse(value));
+				}
+
+				bool valid = true;
+				for (int i = 0; i < pageNumberUpdates.Count; ++i)
+				{
+					int currentPage = pageNumberUpdates[i];
+					var rulesThatApply = rules[currentPage];
+					foreach (var rule in rulesThatApply)
+					{
+						if (currentPage == rule.after)
+						{
+							for (int j = i + 1; j < pageNumberUpdates.Count; ++j)
+							{
+								int obsValue = pageNumberUpdates[j];
+								if (rule.before == obsValue)
+								{
+									valid = false;
+									break;
+								}
+							}
+
+						}
+						if (!valid)
+						{
+							break;
+						}
+					}
+
+					if (!valid)
+					{
+						break;
+					}
+				}
+				if (valid)
+				{
+					continue;
+				}
+
+				// order and find middle
+
+				HashSet<Rule> orderRulesForAllNumbers = [];
+				foreach (var pageNumber in pageNumberUpdates)
+				{
+					foreach (var rule in rules[pageNumber])
+					{
+						orderRulesForAllNumbers.Add(rule);
+					}
+				}
+
+
+				RuleComparer ruleComparer = new(orderRulesForAllNumbers);
+				pageNumberUpdates.Sort(ruleComparer);
+
+				int middleIndex = pageNumberUpdates.Count / 2;
+				int middleValue = pageNumberUpdates[middleIndex];
+				sum += middleValue;
+
+			}
+			else
+			{
+				List<string> values = line.Split("|").ToList();
+				var before = int.Parse(values[0]);
+				var after = int.Parse(values[1]);
+				Rule newRule = new(before, after);
+				if (!rules.ContainsKey(before))
+				{
+					rules.Add(before, []);
+				}
+				rules[before].Add(newRule);
+				if (!rules.ContainsKey(after))
+				{
+					rules.Add(after, []);
+				}
+				rules[after].Add(newRule);
+
+			}
+
+		}
+		Console.WriteLine(sum);
 	}
 }
