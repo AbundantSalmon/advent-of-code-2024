134f6e90ed095fe25fdde57fc946433ed3c081b7
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2024-12-07 23:03:28 +0800

    prepare day 08

---
+++ Day08/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Day08;
+
+internal static class Day08
+{
+    public static void Main(string[] args)
+    {
+        const string inputFile = "input.txt";
+
+        Console.WriteLine("Day08!");
+
+        List<string> lines = [];
+
+        try
+        {
+            StreamReader sr = new(inputFile);
+            string? line = sr.ReadLine();
+            while (line != null)
+            {
+                lines.Add(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: Day08 <part 1/2>");
+            return;
+        }
+
+        string input = args[0];
+        int day = int.Parse(input);
+        switch (day)
+        {
+            case 1:
+                Part1(lines);
+                break;
+            case 2:
+                Part2(lines);
+                break;
+            default:
+                Console.WriteLine("Usage: Day08 <part 1/2");
+                break;
+        }
+    }
+
+
+    private static void Part1(List<string> lines)
+    {
+        Console.WriteLine("Part 1");
+    }
+
+    private static void Part2(List<string> lines)
+    {
+        Console.WriteLine("Part 2");
+    }
+}
